96b3041d1ca93a479402cf4d25746217e7efe7ad
adhusson <pangel.neu@gmail.com>
2020-06-01 14:37:42 +0200

    make sandbox.mli readable

--- lib/sandbox.mli
+++ lib/sandbox.mli
@@ -1,76 +1,73 @@
 module Make () : sig
 
-  (* Initialize blockchain state *)
+  (* Resources *)
+  val token : string -> token
+  val user : ?defaults:bool -> string -> user
 
-  module P : module type of Env.Imp.Program
-  module C : module type of Env.Imp.Chain
+  val users : string list -> user list
+  val google : token
+  val euro : token
+  val alice : user
+  val bob : user
+  val carol : user
 
+  (* Type synonyms *)
   type pos = Address.t
   type user = Address.t
   type contract = Address.t
   type token = Address.t
   type box = Address.t
 
-  val box : pos -> box
-
-  val init : user -> string -> pos
-
-  val prev : pos -> pos
-
-  val grow : pos -> contract -> string -> pos
-
-  val time : unit -> int
-  val time_set : int -> unit
-
-  val first : unit -> bool
-
-  val last : unit -> bool
-
+  (* Moving funds *)
   type asset = Token of Dec.token * Dec.amount | Position of Dec.pos
   val (~$) : int -> token -> asset
   val (~@) : pos -> asset
 
   val give : asset -> Address.t -> unit
-
-  val pay : ?from:Address.t -> ?upto:bool -> asset -> Address.t -> unit
-
   val transfer : Address.t -> asset -> Address.t -> unit
-
   val send : Address.t -> int -> token -> Address.t -> unit
+  val swap : Address.t -> asset -> Address.t -> asset -> unit
+  val fund_left : ?from:Address.t -> asset -> pos -> unit
+  val fund_right : ?from:Address.t -> asset -> pos -> unit
 
-  (* address can be token or pos *)
+  (* External tradeline manipulation *)
+  val init : user -> string -> pos
+  val grow : pos -> contract -> string -> pos
+  val segment : string -> ?on_connect:(pos->pos -> unit) -> ?pull:(pos->pos->unit) -> ?commit:(pos->pos->unit) -> unit -> contract
+  
+  (* Utility *)
+  val box : pos -> box
+  val prev : pos -> pos
+
+  (* Functions used inside segments *)
+  val first : unit -> bool
+  val last : unit -> bool
+  val pay : ?from:Address.t -> ?upto:bool -> asset -> Address.t -> unit
   val provision : Address.t -> Address.t -> int
-
   val reduce : unit -> unit
 
-  val segment : string -> ?on_connect:(pos->pos -> unit) -> ?pull:(pos->pos->unit) -> ?commit:(pos->pos->unit) -> unit -> contract
+  (* Payoff calculation *)
+  val set_payoffs_baseline : unit -> unit
+  val payoffs : compact:bool -> pos -> unit
 
-val swap : Address.t -> asset -> Address.t -> asset -> unit
+  (* Oracles *)
+  val oracle : (int*int) list -> Address.t
+  val consult : Address.t -> int
 
-val set_payoffs_baseline : unit -> unit
+  (* Tradeline cannels *)
+  type 'a chan
+  val forward_channel : ?accumulate:('a -> 'a -> 'a) -> 'a -> 'a chan
+  val backward_channel : ?accumulate:('a -> 'a -> 'a) -> 'a -> 'a chan
+  val read : 'a chan -> 'a
+  val write : 'a chan -> 'a -> unit
+  val shift : 'a chan -> unit
+  
+  (* Access to low-level functions *)
+  module P : module type of Env.Imp.Program
+  module C : module type of Env.Imp.Chain
 
-val payoffs : compact:bool -> pos -> unit
-
-val fund_left : ?from:Address.t -> asset -> pos -> unit
-val fund_right : ?from:Address.t -> asset -> pos -> unit
-
-val token : string -> token
-val user : ?defaults:bool -> string -> user
-val oracle : (int*int) list -> Address.t
-val consult : Address.t -> int
-val users : string list -> user list
-val google : token
-val euro : token
-val alice : user
-val bob : user
-val carol : user
-
-type 'a chan
-
-val forward_channel : ?accumulate:('a -> 'a -> 'a) -> 'a -> 'a chan
-val read : 'a chan -> 'a
-val write : 'a chan -> 'a -> unit
-val shift : 'a chan -> unit
+  (* State manipulation *)
+  val time : unit -> int
+  val time_set : int -> unit
+  
 end
-
-
